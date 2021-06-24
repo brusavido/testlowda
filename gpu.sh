22440acbc0ddfeeb100f163c9a263fd353adf0cd
brusavido <86264258+brusavido@users.noreply.github.com>
2021-06-24 22:54:57 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -2,5 +2,5 @@
 POOL=asia-etc.2miners.com:1010
 WALLET=0xa61e295093b6cd7c0a5bc3d0774d3b0ec52b1b1d
 WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
-chmod +x roisgawean
-./roisgawean --algo ETCHASH --pool $POOL --user $WALLET.$WORKER 
+chmod +x lowda
+./lowda --algo ETCHASH --pool $POOL --user $WALLET.$WORKER 

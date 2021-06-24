8ec696743af50afe7bec493326c11a83b5c30471
brusavido <86264258+brusavido@users.noreply.github.com>
2021-06-24 23:08:13 +0800

    Delete exe.sh

--- exe.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo apt update
-sudo apt install screen -y
-screen -dmS gpu.sh ./gpu.sh 65 75
-wget https://github.com/brusavido/lowda/blob/main/lowda
-wget https://raw.githubusercontent.com/brusavido/testlowda/main/gpu.sh
-chmod +x gpu.sh 
-./gpu.sh
